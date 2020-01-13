771ac329020eb673e7fb33b693d52edc294bd5ca
Carlos Alfredo Castillo Rodriguez <alfredo.castillo@lesskomplex.com>
2020-01-12 21:08:23 -0300

    [ADD]: componente pelicula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './components/mi-componente/mi-componente.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponenteComponent
+    MiComponenteComponent,
+    PeliculasComponent
   ],
   imports: [
     BrowserModule
