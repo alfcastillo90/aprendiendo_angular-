8991429edf9931c3265f82d96c1c3f05a712b6d8
Carlos Alfredo Castillo Rodriguez <alfredo.castillo@lesskomplex.com>
2020-01-12 23:51:38 -0300

    [ADD]: hooks para aprender el ciclo de vida de los componentes

--- src/app/components/mi-componente/mi-componente.component.ts
+++ src/app/components/mi-componente/mi-componente.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-mi-componente',
@@ -9,12 +10,20 @@ export class MiComponenteComponent {
   public titulo: string;
   public comentario: string;
   public year: number;
+  public mostrarPeliculas: boolean;
 
   constructor() {
-    this.titulo = 'Hola mundo,soy un componente';
     this.comentario = 'Este es mi primer componente';
+    this.mostrarPeliculas = true;
+    this.titulo = 'Hola mundo,soy un componente';
     this.year = 2020;
     console.log('Mi componente cargado');
     console.log(this.titulo, this.comentario, this.year);
+    console.log(this.mostrarPeliculas);
   }
+
+  ocultarPeliculas() {
+    this.mostrarPeliculas = false;
+  }
+
 }
--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-peliculas',
   templateUrl: './peliculas.component.html',
   styleUrls: ['./peliculas.component.css']
 })
-export class PeliculasComponent implements OnInit {
+export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
-  constructor() { }
+  public titulo: string;
+
+  constructor() {
+    this.titulo = 'Componente peliculas';
+    console.log('componente lanzado');
+  }
 
   ngOnInit() {
+    console.log('componente iniciado');
+  }
+
+  ngDoCheck() {
+    console.log('do check lanzado');
+  }
+
+  cambiarTitulo() {
+    this.titulo = 'El título ha sido cambiado';
+  }
+
+  ngOnDestroy() {
+    console.log('el componente se va a eliminar de la ejecucion');
   }
 
 }
