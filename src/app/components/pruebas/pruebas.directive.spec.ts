c3b4272f37e2da7b8adf8e8c074758ee02e15045
Carlos Alfredo Castillo Rodriguez <alfredo.castillo@lesskomplex.com>
2020-01-19 14:32:41 -0300

    [ADD]: archivo de rutas y componentes de pruebas

---
+++ src/app/components/pruebas/pruebas.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PruebasDirective } from './pruebas.directive';
+
+describe('PruebasDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PruebasDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/components/pruebas/pruebas.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appPruebas]'
+})
+export class PruebasDirective {
+
+  constructor() { }
+
+}
