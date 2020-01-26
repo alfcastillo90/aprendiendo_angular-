abe0c3fd9ec3152ae1b670980fc67cfab37d77bc
alfcastillo90 <cacr1990@gmail.com>
2020-01-26 12:18:04 -0300

    [ADD]: modelo de peliculas

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
-
+import { Pelicula } from '../../models/peliculas';
 @Component({
   selector: 'app-peliculas',
   templateUrl: './peliculas.component.html',
@@ -8,17 +8,17 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
   public titulo: string;
-  public peliculas: Array<any>;
+  public peliculas: Pelicula[];
 
   constructor() {
     this.titulo = 'Componente peliculas';
     this.peliculas =  [
       // tslint:disable-next-line:max-line-length
-      {year: 2016, title: 'Los vengadores: Endgame', image: 'https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg'},
+      new Pelicula('Los vengadores: Endgame', 2016, 'https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg'),
       // tslint:disable-next-line:max-line-length
-      {year: 2017, title: 'Spiderman 4', image: 'https://as.com/meristation/imagenes/2019/08/25/noticias/1566717212_191664_1566717436_noticia_normal.jpg'},
+      new Pelicula('Spiderman 4', 2017, 'https://as.com/meristation/imagenes/2019/08/25/noticias/1566717212_191664_1566717436_noticia_normal.jpg'),
       // tslint:disable-next-line:max-line-length
-      {year: 2018, title: 'Batman vs Superman', image: 'https://vignette.wikia.nocookie.net/doblaje/images/d/de/Batman_vs_Superman_Poster_Final.png/revision/latest?cb=20160326040156&path-prefix=es'}
+      new Pelicula('Batman vs Superman', 2018, 'https://vignette.wikia.nocookie.net/doblaje/images/d/de/Batman_vs_Superman_Poster_Final.png/revision/latest?cb=20160326040156&path-prefix=es')
     ];
   }
 
